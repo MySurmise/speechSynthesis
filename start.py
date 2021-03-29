317b2203c27f84adeb800503d5e9078357cec3db
MySurmise <marlbos@outlook.de>
2021-03-29 15:26:20 +0200

    start

---
+++ start.py
@@ -0,0 +1,9 @@
+"ˈlaʊ̯tʃpraːxə"
+
+# Fahrplan:
+
+# Lautschrift von Duden crawlen
+# Für jeden Laut Ton einsprechen
+# Bestimmte Akzente wie ' beachten -> Ton manipulieren
+# Ausgabe aneinanderreihen und Enden überlappen lassen -> cleaner
+# Wellen "mischen"? -> Keine Pausen zwischen Buchstaben
